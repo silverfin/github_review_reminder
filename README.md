e11770155f50e8fdfcc6aa873bfb6643dc96069a
Andrew Fecheyr <andrew@bedesign.be>
2017-08-09 15:40:46 +0200

    Count reviews requested and post to slack

--- README.md
+++ README.md
@@ -9,16 +9,15 @@ At [Silverfin](https://github.com/GetSilverfin), this is run each morning to rem
 Run it as a docker image:
 
 ```shell
-docker run --rm -e GITHUB_TOKEN=<your-gh-token> -e REPO=<your-gh-repo> GetSilverfin/github_review_reminder
+docker run --rm -e GITHUB_TOKEN=<your-gh-token> -e GITHUB_REPO=<your-gh-repo> -e SLACK_WEBHOOK_URL="<your-slack-webhook-url>" GetSilverfin/github_review_reminder
 ```
 
 ## Configuration
 
 Configuration is done through environment variables. There variables are available:
 
-* GITHUB_TOKEN: an oauth token for the github api. Either set this or GITHUB_USER and GITHUB_PASSWORD
+* GITHUB_TOKEN: a [token for the github api](https://github.com/blog/1509-personal-api-tokens). Either set this or GITHUB_USER and GITHUB_PASSWORD
 * GITHUB_USER: the github username used for the github api
 * GITHUB_PASSWORD: the password for GITHUB_USER
 * GITHUB_REPO: the github repository name this should run against, eg: "GetSilverfin/github_review_reminder"
-* SLACK_CHANNEL: (optional) the slack channel name to post the message
-* SLACK_TOKEN: (optional) the api token for posting to Slack
+* SLACK_WEBHOOK_URL: (optional) the slack [incoming webhook url](https://my.slack.com/services/new/incoming-webhook/) used to post the message
--- run.rb
+++ run.rb
@@ -13,21 +13,56 @@ gh_authentication =
 
 gh_client = Octokit::Client.new(gh_authentication)
 
+# Fetch all open PRs
 prs = gh_client.pull_requests(ENV['GITHUB_REPO'], state: :open)
 
-# Get labels for issues
-
+# Get labels via issues
 label_counts = prs.each_with_object(Hash.new(0)) do |pr, hash|
   issue = gh_client.issue(ENV['GITHUB_REPO'], pr.number)
   labels = issue.labels.map(&:name)
   labels.each { |label| hash[label] += 1 }
 end
 
+# Get reviewers for each pr
+reviews_requested = prs.each_with_object(Hash.new([])) do |pr, hash|
+  usernames = gh_client.pull_request_review_requests(ENV['GITHUB_REPO'], pr.number).map(&:login)
+  usernames.each { |username| hash[username] += [pr.number] }
+end
+
+# Get counts for easy sorting
+reviews_requested_count = reviews_requested.map do |username, prs|
+  [username, prs.count]
+end.to_h
+
+# Post message to Slack
+def slack(text)
+  puts "sending to slack"
+  uri = URI(ENV['SLACK_WEBHOOK_URL'])
+  payload = {"text" => text}
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  request = Net::HTTP::Post.new(uri.request_uri, "Content-type" => "application/json")
+  request.body = payload.to_json
+  http.request(request)
+end
+
+def slack_format_github_pr_link(pr_number)
+  "<https://github.com/#{ENV['GITHUB_REPO']}/pull/#{pr_number}|##{pr_number}>"
+end
+
 output = ""
 
 output << "Open PRs: #{prs.count}\n"
 label_counts.to_a.map(&:reverse).sort.reverse.each do |count, label|
-  output << "#{count} have label \"#{label}\"\n"
+  output << "#{count} with label *#{label}*\n"
+end
+output << "\n"
+reviews_requested_count.to_a.map(&:reverse).sort.reverse.each do |count, username|
+  output << "#{count} reviews requested of *#{username}*: #{reviews_requested[username].map { |pr_number| slack_format_github_pr_link(pr_number) }.join(", ")}\n"
 end
 
 puts output
+
+if ENV['SLACK_WEBHOOK_URL']
+  slack(output)
+end
